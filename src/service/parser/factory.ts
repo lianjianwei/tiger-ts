2752873efeef8c6f7fe75e1d1a5cda619bc03e6d
ljw <3478289084@qq.com>
2024-11-18 23:52:37 +0800

    fix: 优化ParserFactory构造函数传参

--- src/service/parser/factory.ts
+++ src/service/parser/factory.ts
@@ -1,20 +1,20 @@
-import { IParser, ParserFactoryBase, Type } from '../../contract';
+import { IParser, ParserFactoryBase } from '../../contract';
 
 export class ParserFactory extends ParserFactoryBase {
 
     public constructor(
         private m_Parser: {
-            [type: string]: Type<IParser>;
+            [type: string]: IParser;
         }
     ) {
         super();
     }
 
     public build<T>(type: string): IParser<T> {
-        const typer = this.m_Parser[type];
-        if (!typer)
+        const parser = this.m_Parser[type];
+        if (!parser)
             throw new Error(`ParserFactory 未提供 [${type}] 类型解析器`);
 
-        return new typer();
+        return parser;
     }
 }
