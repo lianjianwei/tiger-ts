f0a1d8fb1f9dc8d0302bb0e8d2856a1068fb2f32
连建伟 <3478289084@qq.com>
2025-10-24 14:51:21 +0800

    feat: 优化Mongo的close函数

--- src/service/mongo/db-factory.ts
+++ src/service/mongo/db-factory.ts
@@ -6,14 +6,25 @@ import { BuilderOption, DbFactoryBase, DbModel, IDbRepository, IUnitOfWork } fro
 
 export class MongoDbFactory extends DbFactoryBase {
 
-    private m_MongoClientMap: {
+    private m_SrvMongoClientMap: {
         [srvNo: number]: Promise<MongoClient>
+    } = {};
+
+    private m_UrlMongoClientMap: {
+        [url: string]: {
+            client: MongoClient,
+            count: number;
+        }
+    } = {};
+
+    private m_SrvUrlMap: {
+        [srvNo: number]: string;
     } = {};
 
     public constructor(
         private m_Url: string,
         private m_DbName: string,
-        private m_GetMongoClient: (srvNo: number) => Promise<MongoClient>,
+        private m_GetMongoUrl: (srvNo: number) => Promise<string>,
         private m_BulkWriteOptions: BulkWriteOptions = {},
         private m_Option?: MongoClientOptions
     ) {
@@ -22,16 +33,22 @@ export class MongoDbFactory extends DbFactoryBase {
 
     public getOriginConnection<T>(srvNo?: number) {
         srvNo ??= 0;
-        this.m_MongoClientMap[srvNo] ??= new Promise<MongoClient>(async (s, f) => {
+        this.m_SrvMongoClientMap[srvNo] ??= new Promise<MongoClient>(async (s, f) => {
             try {
-                const client = srvNo ? await this.m_GetMongoClient(srvNo) : new MongoClient(this.m_Url, this.m_Option);
-                await client.connect();
-                s(client);
+                const url = srvNo ? await this.m_GetMongoUrl(srvNo) : this.m_Url;
+                this.m_UrlMongoClientMap[url] ??= {
+                    client: new MongoClient(url, this.m_Option),
+                    count: 0,
+                };
+                this.m_SrvUrlMap[srvNo] = url;
+                this.m_UrlMongoClientMap[url].count++;
+                await this.m_UrlMongoClientMap[url].client.connect();
+                s(this.m_UrlMongoClientMap.client[url]);
             } catch (err) {
                 f(err);
             }
         });
-        return this.m_MongoClientMap[srvNo] as Promise<T>;
+        return this.m_SrvMongoClientMap[srvNo] as Promise<T>;
     }
 
     public build<T extends DbModel>(opt: BuilderOption<T>): IDbRepository<T> {
@@ -43,11 +60,18 @@ export class MongoDbFactory extends DbFactoryBase {
     }
 
     public async close(srvNo: number) {
-        if (this.m_MongoClientMap[srvNo]) {
-            const mongClientPromise = this.m_MongoClientMap[srvNo];
-            delete this.m_MongoClientMap[srvNo];
-            const mongClient = await mongClientPromise;
-            await mongClient.close();
+        if (this.m_SrvMongoClientMap[srvNo]) {
+            const url = this.m_SrvUrlMap[srvNo];
+            delete this.m_SrvMongoClientMap[srvNo];
+            delete this.m_SrvUrlMap[srvNo];
+            const urlMongoClient = this.m_UrlMongoClientMap[url];
+            if (urlMongoClient) {
+                urlMongoClient.count--;
+                if (urlMongoClient.count <= 0) {
+                    delete this.m_UrlMongoClientMap[url];
+                    await urlMongoClient.client.close();
+                }
+            }
         }
     }
 
